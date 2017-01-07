548c47c81fa62a9feefa2bc711a49153673df271
Matthew McCord <mattm.eternal@gmail.com>
2017-01-07 16:31:46 -0600

    I give up on the one line to print them all xD

--- cars.py
+++ cars.py
@@ -40,26 +40,26 @@ available_car_colors = (
   (15, 1), (15, 4), (15, 7)
 )
 
-example_dict = {
-    'Toyota': {
-      'Prius': ['Charcoal', 'Brick', 'Ivory'],
-      'Camry': ['Black', 'Red', 'White']
-    },
-    'Nissan': {
-      'Sentra': ['Charcoal', 'Blue', 'Ivory'], 
-      'Altima': ['Black', 'Charcoal', 'White'], 
-      'Xterra': ['Charcoal', 'Navy', 'Ivory']
-    }
-}
 # STEP 1 GRAB MAKES
 # make[1] for make in makes
 
 # STEP 2 GRAB MODELS
 # for model in models if model[2] == make[0]
 
-# STEP 3 LOOP THROUGH AVAILABLE COLORS AND COLOR CODES TO FIND MATCHES
+# STEP 3 LOOP THROUGH AVAILABLE COLORS AND COLOR CODES TO FIND MATCHES AND BUILD LIKE DICTIONARY
 # color[1] for available_car_color in available_car_colors for color in colors if model[0] == available_car_color[0] if available_car_color[1] == color[0]
-car_dict = {
+# {
+#   make: {
+#     model: [color, color, color]
+#     model: [color, color, color]
+#     },
+#     model: [color, color, color]
+#     model: [color, color, color]
+#     }
+#     ...
+# }
+
+car_dicto = {
   make[1]:
     {
       model[1]: 
@@ -70,7 +70,7 @@ car_dict = {
   }
 
 # STEP 4 PRINT IT OUT
-for k, v in car_dict.items():
+for k, v in car_dicto.items():
   print('\n' + k + '\n-----------------------------')
   for model in v:
     print("{0} available in {1}".format(model, ', '.join(v[model])))
